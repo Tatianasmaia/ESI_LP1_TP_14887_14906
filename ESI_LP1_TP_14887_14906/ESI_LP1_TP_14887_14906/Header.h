3c47963750c515c6ab371d7327c1c66f4190b0cf
Tatianasmaia <a14887@alunos.ipca.pt>
2021-01-04 15:17:54 +0000

    Commit

--- ESI_LP1_TP_14887_14906/ESI_LP1_TP_14887_14906/Header.h
+++ ESI_LP1_TP_14887_14906/ESI_LP1_TP_14887_14906/Header.h
@@ -1,1 +1,30 @@
 #pragma once
+/*Estrutura para guardar a informação dos concorrentes*/
+typedef struct concorrente {
+
+	int id;
+	char nome[50];
+	char marca[50];
+
+}Concorrente;
+
+/*Estrutura para guardar a informação das etapas*/
+typedef struct etapa {
+
+	char inicio[50];
+	char fim[50];
+	float distancia;
+
+}Etapa;
+
+/*Estrutura para guardar a informação de uma prova*/
+typedef struct prova {
+
+	int idConcorrente;
+	char etapa1[50];
+	char etapa2[50];
+	int tempo;
+	int nEtapas;
+	int nConcorrentes;
+
+}Prova;
--- ESI_LP1_TP_14887_14906/ESI_LP1_TP_14887_14906/main.c
+++ ESI_LP1_TP_14887_14906/ESI_LP1_TP_14887_14906/main.c
@@ -1,8 +1,90 @@
 #include <stdio.h>
+#include "Header.h"
+
+#pragma warning(disable : 4996)
 
 int main() {
 
-	FILE *f;
+	FILE* f = fopen("DistanciaEtapas.txt", "r");
+
+	//declaração e inicialização de variáveis
+	char linha[500] = { 0 };
+	char* token;
+	char* aux[3] = { 0 };
+	int contador = 0;
+
+	Etapa novaEtapa[3];
+	
+	int j = 0;
+	while (fgets(linha, sizeof(linha), f))
+	{
+		int i = 0;
+
+		
+		if ((linha[0] >= 'a' && linha[0] <= 'z') || (linha[0] >= 'A' && linha[0] <= 'Z')) {
+
+
+			token = strtok(linha, ";");
+			aux[0] = token;
+			i++;
+
+			while (token != NULL)
+			{
+				token = strtok(NULL, ";");
+
+				if (token) {
+					aux[i] = strdup(token);
+				
+					i++;
+				}
+			}
+
+			
+			strcpy(novaEtapa[j].inicio, aux[0]);
+			strcpy(novaEtapa[j].fim, aux[1]);
+			novaEtapa[j].distancia = atoi(aux[2]);
+		}
+
+		j++;
+
+			
+	}
+	for (int x = 0; x < 3; x++) {
+		printf("%s, %s, %f\n", novaEtapa[x].inicio, novaEtapa[x].fim, novaEtapa[x].distancia);
+	}
+
+	fclose(f);
+}
+
+    /*FILE* fp = fopen("DistanciaEtapas.txt", "r");
+    const char s[3] = ";";
+    char* token;
+    int i;
+    if (fp != NULL)
+    {
+        char line[20];
+        while (fgets(line, sizeof line, fp) != NULL)
+        {
+            token = strtok(line, s);
+            for (i = 0; i < 3; i++)
+            {
+                if (i == 0)
+                {
+                    printf("%s\t", token);
+                    token = strtok(NULL, s);
+                }
+                else {
+                    printf("%d\n", atoi(token));
+                }
+            }
+        }
+        fclose(fp);
+    }
+    else {
+        perror("DadosConcorrentes.txt");
+    }*/
+
+	/*FILE *f;
 	f = open("DadosConcorrentes.txt", "r");
 
 	if (f == NULL) {
@@ -24,5 +106,5 @@ int main() {
 
 		
 	}
-	return 0;
-}
+	return 0;*/
+//}
